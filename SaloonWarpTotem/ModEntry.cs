de2e53374615bcc41f442cb69233a554eecddfdf
Artinity <31357507+Artinity@users.noreply.github.com>
2023-04-26 14:05:22 +0200

    Initial commit

---
+++ SaloonWarpTotem/ModEntry.cs
@@ -0,0 +1,34 @@
+﻿using StardewModdingAPI.Events;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace ClassLibrary1
+{
+    public class ModEntry : Mod
+    {
+        /// <inheritdoc />
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        /// <summary>
+        /// Raised after the player presses a button on the keyboard, controller, or mouse.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady || e.Button != SButton.MouseRight)
+            {
+                return;
+            }
+
+            if (Game1.player.ActiveObject?.ParentSheetIndex == 346 // Beer
+                && Game1.player.ActiveObject.Stack == 12)
+            {
+                Game1.player.warpFarmer(new Warp(0, 0, "Saloon", 34, 18, false));
+            }
+        }
+    }
+}
